7e4e980867d2f30ea19d1ee1723147bd1dd47feb
olenasniatynska <92575026+olenasniatynska@users.noreply.github.com>
2022-12-08 21:38:15 +0200

    Update TestFlowerbucket.java

--- src/test/TestFlowerbucket.java
+++ src/test/TestFlowerbucket.java
@@ -19,53 +19,5 @@ public class FlowerBucketTest {
     }
 
     @Test
-    void testPrice() {
-        assertEquals(100, flowerBucket.getPrice());
-    }
-
-    @Test
-    void testToString() {
-        assertEquals("FlowerBucket{flowerPacks=[Flower pack: Flower: ROSE, price: 10.0," +
-                " sepal length: 0.0, color: null, count: 10], price=100.0, flowerCount=10}",
-                flowerBucket.toString());
-    }
-
-    @Test
-    void testAddFlowerPack() {
-        Flower flower = new Rose();
-        flower.setPrice(10);
-        FlowerPack flowerPack = new FlowerPack(flower, 10);
-        flowerBucket.addFlowerPack(flowerPack);
-        assertEquals(200, flowerBucket.getPrice());
-    }
-
-    @Test
-    void testGetFlowerPacks() {
-        Flower flower = new Rose();
-        flower.setPrice(10);
-        FlowerPack flowerPack = new FlowerPack(flower, 10);
-        flowerBucket.addFlowerPack(flowerPack);
-        assertEquals(2, flowerBucket.getFlowerPacks().size());
-    }
-
-    @Test
-    void testGetPrice() {
-        assertEquals(100, flowerBucket.getPrice());
-    }
-
-    @Test
-    void testGetFlowerCount() {
-        assertEquals(10, flowerBucket.getFlowerCount());
-    }
-
-    @Test
-    void testConstructor() {
-        Flower flower = new Rose();
-        flower.setPrice(10);
-        FlowerPack flowerPack = new FlowerPack(flower, 10);
-        ArrayList<FlowerPack> flowerPacks = new ArrayList<>();
-        flowerPacks.add(flowerPack);
-        FlowerBucket flowerBucket = new FlowerBucket(flowerPacks);
-        assertEquals(100, flowerBucket.getPrice());
-    }
+        True
 }
