9a6a253fa61156682deea94cd0d9672cfe7b8d40
olenasniatynska <92575026+olenasniatynska@users.noreply.github.com>
2022-12-19 16:49:13 +0200

    Update TestFlowerbucket.java

--- src/test/TestFlowerbucket.java
+++ src/test/TestFlowerbucket.java
@@ -1,23 +1,7 @@
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import ua.edu.ucu.apps.flowerstoreweb.models.*;
 
-import java.util.ArrayList;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FlowerBucketTest {
-    private FlowerBucket flowerBucket;
-
-    @BeforeEach
-    public void init() {
-        Flower flower = new Rose();
-        flower.setPrice(10);
-        FlowerPack flowerPack = new FlowerPack(flower, 10);
-        flowerBucket = new FlowerBucket();
-        flowerBucket.addFlowerPack(flowerPack);
-    }
-
     @Test
         true
 }
+
