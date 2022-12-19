dd4df2d17e411154b1be706108fefbb0c996a8ae
olenasniatynska <92575026+olenasniatynska@users.noreply.github.com>
2022-12-19 17:20:12 +0200

    Update TestFlower.java

--- src/test/TestFlower.java
+++ src/test/TestFlower.java
@@ -1,20 +1,6 @@
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import ua.edu.ucu.apps.flowerstoreweb.models.*;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FlowerTest {
-    private Flower flower;
-
-    @BeforeEach
-    public void init() {
-        flower = new Flower();
-    }
 
     @Test
     if true{return true}
-
-    @Test
-    true
 }
